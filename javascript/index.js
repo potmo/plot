263a8f67c192933c094639e57c5d0abe69ce6474
Nisse Bergman <nisse@potmo.com>
2016-01-14 20:37:16 +0100

    writing everything nicely

--- javascript/index.js
+++ javascript/index.js
@@ -32,7 +32,7 @@ function rasterizeImage(rotation, color, sample) {
   // TODO: stop when we reached the limit of the image
   var y = 0;
   var x = 0;
-  var max = 20;
+  var max = 100;
 
   for (var i = 0; i < max; i++){
 
@@ -42,7 +42,7 @@ function rasterizeImage(rotation, color, sample) {
     var length = 5000; //TODO: get a way to get this from the amplitude
 
     var xtravel = length * strength;
-    var ytravel = length ;
+    var ytravel = 5000;
 
     //TODO: sample to get the length
     positions.push({x: x + xtravel/2, y: y - ytravel / 2});
@@ -75,7 +75,7 @@ function rasterizeImage(rotation, color, sample) {
     v1 = vertices[1];
     v2 = vertices[2];
 
-    var factor = 0.25;
+    var factor = 1.0;
 
     v0.x = v1.x + (v0.x - v1.x) * factor;
     v0.y = v1.y + (v0.y - v1.y) * factor;
@@ -90,6 +90,7 @@ function rasterizeImage(rotation, color, sample) {
   .iterate(function(vertices){
     output += drawLine(vertices[0].x, vertices[0].y, vertices[1].x, vertices[1].y, 2);
     output += drawLine(vertices[1].x, vertices[1].y, vertices[2].x, vertices[2].y, 2);
+    output += drawLine(vertices[0].x, vertices[0].y, vertices[2].x, vertices[2].y, 5);
   })
 
   // draw the incenters
@@ -139,14 +140,51 @@ function rasterizeImage(rotation, color, sample) {
   // draw the arc (http://math.stackexchange.com/questions/285866/calculating-circle-radius-from-two-points-on-circumference-for-game-movement)
   // http://www.wolframalpha.com/input/?i=find+a+in+c+%3D+sqrt%282r%5E2%281%E2%88%92cos%282a%29%29%29
   .iterate(function(slice){
+    /*
     var dx = slice.tangent2.x - slice.tangent1.x;
     var dy = slice.tangent2.y - slice.tangent1.y;
     var chord = Math.sqrt(dx * dx + dy * dy);
     var arcAngle = Math.asin(0.5 * Math.sqrt(Math.pow(chord,2) / Math.pow(slice.inradius, 2))) * 2;
     var arcDegrees = toDegrees(arcAngle) ;
-    //TODO: Find the actual angle to go from
-    output += drawArc(slice.incenter.x, slice.incenter.y, slice.inradius, 0 + 90 + 90 + arcDegrees / 2, arcDegrees + 90 + 90 + arcDegrees / 2, 4);
 
+    var leg1 = {x: slice.vertices[1].x - slice.vertices[0].x, y: slice.vertices[1].y - slice.vertices[0].y };
+    var leg2 = {x: slice.vertices[1].x - slice.vertices[2].x, y: slice.vertices[1].y - slice.vertices[2].y };
+
+    var leg1normal = getNormalized(leg1);
+    var leg2normal = getNormalized(leg2);
+
+    var bisector = {x: leg1normal.x + leg2normal.x, y: leg1normal.y + leg2normal.y};
+    var bisectorAngle = getAngleFromVector(bisector) - 180;
+
+    arcDegrees = 20;
+
+    output += drawLine(slice.incenter.x, slice.incenter.y, slice.incenter.x + cos(bisectorAngle) * 200, slice.incenter.y + sin(bisectorAngle) * 200, 2);
+    */
+
+    output += drawThreePointArc(slice.tangent1.x, slice.tangent1.y, slice.tangent2.x, slice.tangent2.y, slice.incenter.x, slice.incenter.y, 4);
+
+  })
+
+  // draw the legs
+  .iterate(function(slice) {
+    //output += drawLine(slice.vertices[0].x, slice.vertices[0].y, slice.tangent1.x, slice.tangent1.y, 4);
+    //output += drawLine(slice.tangent2.x, slice.tangent2.y, slice.vertices[2].x, slice.vertices[2].y, 4);
+  })
+
+  .collect(2)
+
+  .iterate(function(slices){
+    output += drawLine(slices[0].tangent2.x, slices[0].tangent2.y, slices[1].tangent2.x, slices[1].tangent2.y, 4);
+  })
+
+  .explode()
+
+  .dropFirst()
+
+  .collect(2)
+
+  .iterate(function(slices){
+    output += drawLine(slices[0].tangent1.x, slices[0].tangent1.y, slices[1].tangent1.x, slices[1].tangent1.y, 4);
   })
 
   //positions.reduce([], function())
@@ -159,7 +197,8 @@ function rasterizeLine() {
 }
 
 
-
+// slide over the elements and create tuples of a size
+// [1,2,3,4] = [[1,2], [2,3], [3,4]]
 Array.prototype.slideOver = function(windowSize){
   var output = [];
   this.reduce(function(array, element){
@@ -179,11 +218,50 @@ Array.prototype.slideOver = function(windowSize){
   return output;
 }
 
+// just iterate over all elements
 Array.prototype.iterate = function(callback) {
   return this.map(function(currentValue, index, array){
     callback(currentValue, index, array);
     return currentValue;
   });
+}
+
+// take an array and make tupels of size and return them in an array [1,2,3,4] = [[1,2], [3,4]]
+Array.prototype.collect = function(windowSize){
+  var output = [];
+  this.reduce(function(array, element){
+    array.push(element);
+
+    if (array.length === windowSize){
+      output.push([].concat(array));
+      array = [];
+    }
+
+    return array;
+
+  }, []);
+  return output;
+}
+
+// like flat map. Take all elements of an array that contains 
+// elements and return an array with all the elements elements concatenated
+// [[1,2], [3,4]] = [1,2,3,4]
+Array.prototype.explode = function() {
+  return this.reduce(function(array, element){
+    return array.concat(element);
+  }, []);
+}
+
+// pop an element from the list
+Array.prototype.dropLast = function() {
+  this.pop();
+  return this;
+}
+
+// unshift an element from the list
+Array.prototype.dropFirst = function() {
+  this.shift();
+  return this;
 }
 
 console.log([1,2,3,4,5,6,7,8].slideOver(3).join(', '));
@@ -198,6 +276,29 @@ function drawArc(x, y, radius, fromAngle, toAngle, color) {
   output += 'PU' + startX.toFixed(2) + ',' + startY.toFixed(2) + ';\n';
   output += 'PD;\n';
   output += 'AA' + x.toFixed(2) + ',' + y.toFixed(2) + ',' + arcAngle.toFixed(2) + ';\n';
+  output += 'SP1;\n'
+  return output;
+}
+
+function drawThreePointArc(startX, startY, endX, endY, centerX, centerY, color) {
+
+  var centerToStart = {x: startX - centerX, y: startY - centerY};
+  var centerToEnd = {x: endX - centerX, y: endY - centerY};
+
+  var centerToStartNormal = getNormalized(centerToStart);
+  var centerToEndNormal = getNormalized(centerToEnd);
+
+  var dotProduct = getDot(centerToStartNormal, centerToEndNormal);
+
+  var arcAngle = Math.acos(dotProduct);
+
+  var arcDegrees = toDegrees(-arcAngle);
+
+  var output = '';
+  output += 'SP'+color+';\n'
+  output += 'PU' + startX.toFixed(2) + ',' + startY.toFixed(2) + ';\n';
+  output += 'PD;\n';
+  output += 'AA' + centerX.toFixed(2) + ',' + centerY.toFixed(2) + ',' + arcDegrees.toFixed(2) + ';\n';
   output += 'SP1;\n'
   return output;
 }
@@ -273,6 +374,26 @@ function rotatePointAroundPoint(point, pivot, degrees) {
     y: ty
   };
 }
+
+function getAngleFromVector(vector){
+  var degrees = toDegrees(Math.atan2(vector.x, vector.y));
+  while(degrees < 0) degrees += 360;
+  while(degrees >= 360) degrees -= 360;
+  return degrees;
+}
+
+function getNormalized(vector) {
+  var length = getDistance(0,0, vector.x, vector.y)
+  var normalizedX = vector.x / length;
+  var normalizedY = vector.y / length;
+  return {x: normalizedX, y: normalizedY};
+}
+
+function getDot(vector1, vector2) {
+  return vector1.x * vector2.x + vector1.y * vector2.y;
+}
+
+
 
 function findIncenter(Ax, Ay, Bx, By, Cx, Cy) {
   // http://www.mathopenref.com/coordincenter.html
